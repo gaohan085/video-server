3f9ea3f7db08cdea258d6a80347f21b6cf84e70a
gaohan <gaohan085@gmail.com>
2023-03-09 12:40:21 +0800

    spinner add font-size css propority

--- components/spinner.tsx
+++ components/spinner.tsx
@@ -1,9 +1,10 @@
 import { FcSynchronize } from "react-icons/fc";
 import styled from "styled-components";
 
-const StyledSpinner = styled.span`
+const StyledSpinner = styled.span<{ fontSize?: number }>`
   display: inline-block;
   vertical-align: middle;
+  font-size: ${(props) => props.fontSize + "px"};
   svg {
     animation: rotation 2s linear infinite;
     @keyframes rotation {
@@ -17,9 +18,9 @@ const StyledSpinner = styled.span`
   }
 `;
 
-export default function Spinner() {
+export default function Spinner(props: { fontSize?: number }) {
   return (
-    <StyledSpinner>
+    <StyledSpinner fontSize={props.fontSize}>
       <FcSynchronize />
     </StyledSpinner>
   );
