e4df7834b7bb1fb89a59b31d42390bb048c34820
gaohan <gaohan085@gmail.com>
2023-03-09 14:53:14 +0800

    adjust folder and file name to upper case

--- components/FileSys/index.tsx
+++ components/FileSys/index.tsx
@@ -19,7 +19,7 @@ export const FileElem: React.FC = (props: DirChildElem) => {
         <span>
           <FcVideoFile />
         </span>
-        {props.name}
+        {props.name.toUpperCase()}
       </p>
     </div>
   );
@@ -45,7 +45,7 @@ export const FolderElem: React.FC = (props: DirChildElem) => {
         ) : (
           <span>{isOpen ? <FcOpenedFolder /> : <FcFolder />}</span>
         )}
-        {props.name}
+        {props.name.toUpperCase()}
       </p>
       {error ? (
         <>{"Error Fetch Data"}</>
